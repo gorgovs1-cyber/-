44f327802b5bfe2768e407f971f2f7a283205ed8
Shani Gorgov <gorgovs1@gmail.com>
2026-07-12 11:52:43 +0300

    feat: bigger fonts + contrast + neutral onboarding (remove beauty chips)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'shani-money-v20260870';
+var CACHE = 'shani-money-v20260871';
 
 // Allow the page to force an immediately-waiting worker to activate.
 self.addEventListener('message', function(e){
