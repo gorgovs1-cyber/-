48f566fb4704a9d8bb0eecd2ce6b8fd70f66485d
Shani Gorgov <gorgovs1@gmail.com>
2026-07-26 14:02:47 +0300

    editAmt: click appointment amount to edit (calendar-imported clients have no price)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'shani-money-v20260873';
+var CACHE = 'shani-money-v20260874';
 
 // Allow the page to force an immediately-waiting worker to activate.
 self.addEventListener('message', function(e){
